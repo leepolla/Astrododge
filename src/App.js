3d44b4e62926f7ad6a2c40f32b771660eda877bf
Allison Wolf <allisw4@uw.edu>
2016-12-03 18:11:23 -0800

    added a header image and meet the team section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './outerPage/outerPage.css';
-import './outerPage/header.JPG';
 
 class App extends Component {
   render() {
