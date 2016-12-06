819d2b9045f6cde3f01e028b1b7941fea0d46581
Lee Polla <leepolla@gmail.com>
2016-12-06 14:40:09 -0800

    added firebase functions from previous work into leaderboard

--- src/leaderboard/leaderboard.js
+++ src/leaderboard/leaderboard.js
@@ -1,5 +1,41 @@
+var database = firebase.database();
+var scoresData = database.ref('Scores');
+var currentScores = [];
+
 class Leaderboard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            scores: []
+        };
+
+        scoresData.on('value', snapshot => {
+        this.state.scores = snapshot.val();
+        //this.forceUpdate();
+        });
+    }
+
+    //Reads movie data from firebase
+    componentDidMount() {    
+            this.unMountChilAdded = scoresData.on('child_added', function(data) {
+                var highScore = {userName: data.val().userName, scoreValue: data.val().scoreValue, keyID: data.key};
+                currentScores.push(highScore);  
+            });        
+            this.forceUpdate();            
+        }
     
+    //Re-renders cart to reflect changes in firebase data
+    forceUpdate(data) {
+        this.setState({
+            scores: currentScores
+        });
+    }
+
+
+
+
+
     render() {
 
         return (
