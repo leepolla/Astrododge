f52f2520b68272421f631d9cbf8a9a0a26fc279d
lee polla <leepolla@uw.edu>
2016-12-07 16:55:53 -0800

    fixing merges again

---
+++ src/app/directions.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+
+class Directions extends Component {
+     render() {
+          return (
+             <div className="container">
+                  <h3>How to play</h3>
+                 <p>Click Alex's face to start</p>
+              </div>
+          )
+      }
+  }
+  
+ export default Directions; 
